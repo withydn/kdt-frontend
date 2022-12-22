9d55f24e4afa4b3ffb29490f0545559a575c2980
withydn <withydn@naver.com>
2022-12-22 21:14:14 +0900

    a1222

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import styles from './Login.module.css';
+import React from "react";
+import styles from "./Login.module.css";
+import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
@@ -11,16 +12,24 @@ export default function Login() {
           </div>
           <ul className={styles.login_input}>
             <li>
-              <input className={styles.login_inputbox1} type='email' placeholder='아이디(이메일 계정)'></input>
+              <input
+                className={styles.login_inputbox1}
+                type="email"
+                placeholder="아이디(이메일 계정)"
+              ></input>
             </li>
             <li>
-              <input className={styles.login_inputbox2} type='password' placeholder='비밀번호를 입력하세요'></input>
+              <input
+                className={styles.login_inputbox2}
+                type="password"
+                placeholder="비밀번호를 입력하세요"
+              ></input>
             </li>
           </ul>
           <button className={styles.login_btn}>로그인</button>
-          <button className={styles.register_btn} to='/signup'>
-            회원가입
-          </button>
+          <Link to="/signup">
+            <button className={styles.register_btn}>회원가입</button>
+          </Link>
         </div>
         <footer className={styles.footer}></footer>
       </div>
--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import styles from './SignUp.module.css';
+import React from "react";
+import styles from "./SignUp.module.css";
 export default function SignUp() {
   return (
     <div className={styles.container}>
@@ -10,37 +10,52 @@ export default function SignUp() {
         <ul className={styles.signup_input}>
           <li>
             <div className={styles.signup_boxtext}>이름</div>
-            <input type='text' className={styles.signup_box1}></input>
+            <input
+              type="text"
+              className={styles.signup_box1}
+              placeholder="이름을 입력하세요"
+            ></input>
           </li>
           <li>
             <div className={styles.signup_boxtext}>이메일</div>
-            <input type='email' className={styles.signup_box1}></input>
+            <input
+              type="email"
+              className={styles.signup_box1}
+              placeholder="이메일을 입력하세요"
+            ></input>
           </li>
           <li>
             <div className={styles.signup_boxtext}>비밀번호</div>
-            <input type='password' className={styles.signup_box1}></input>
+            <input
+              type="password"
+              className={styles.signup_box1}
+              placeholder="비밀번호를 입력하세요"
+            ></input>
           </li>
           <li>
-            <h2>관심여행지</h2>
-            <select name='tourli'>
-              <option value=''>선택</option>
-              <option value='강원'>강원</option>
-              <option value='경기'>경기</option>
-              <option value='경남'>경남</option>
-              <option value='경북'>경북</option>
-              <option value='경주'>경주</option>
-              <option value='대구'>대구</option>
-              <option value='대전'>대전</option>
-              <option value='부산'>부산</option>
-              <option value='서울'>서울</option>
-              <option value='울산'>울산</option>
-              <option value='인천'>인천</option>
-              <option value='전남'>전남</option>
-              <option value='전북'>전북</option>
-              <option value='제주'>제주</option>
-              <option value='충남'>충남</option>
-              <option value='충북'>충북</option>
+            <div className={styles.signup_boxtext}>관심여행지</div>
+            <select className={styles.tourli}>
+              <option value="">선택</option>
+              <option value="강원">강원</option>
+              <option value="경기">경기</option>
+              <option value="경남">경남</option>
+              <option value="경북">경북</option>
+              <option value="경주">경주</option>
+              <option value="대구">대구</option>
+              <option value="대전">대전</option>
+              <option value="부산">부산</option>
+              <option value="서울">서울</option>
+              <option value="울산">울산</option>
+              <option value="인천">인천</option>
+              <option value="전남">전남</option>
+              <option value="전북">전북</option>
+              <option value="제주">제주</option>
+              <option value="충남">충남</option>
+              <option value="충북">충북</option>
             </select>
+          </li>
+          <li>
+            <button className={styles.signup_btn}>회원가입</button>
           </li>
         </ul>
       </div>

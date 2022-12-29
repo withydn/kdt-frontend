b3c19bd9cbe5134348b790e706abbb78dd35edeb
2Sodol <104712554+dex7688@users.noreply.github.com>
2022-12-30 01:17:16 +0900

    wooseok_12_30

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { fetchDetailInfo } from '../../store/modules/fetchDetailInfo';
 import Kakao from '../../components/Kakao';
 import LoadingSpinner from '../../components/LoadingSpinner.jsx/LoadingSpinner';
+import { TfiBook, TfiLocationPin, TfiThumbUp } from 'react-icons/tfi';
 
 export default function Detail() {
   const { infoData, infoLoading } = useSelector((state) => state.fetchDetailInfo);
@@ -60,28 +61,69 @@ export default function Detail() {
   console.log(infoData);
   return (
     <section className={styles.container}>
-      <h2>{infoData[0]?.title}</h2>
+      <h2 className={styles.sectionTitle}>{infoData[0]?.title}</h2>
       {infoLoading && <LoadingSpinner />}
       {infoData?.map((data) => (
-        <div key={data.contentid}>
-          <div className={styles.infoWrapper}>
+        <div className={styles.detailWrapper} key={data.contentid}>
+          <div className={styles.imageWrapper}>
             <img className={styles.image} src={data.firstimage || '/images/zoom.png'} alt={data.title} />
-            <div className={styles.text}>
-              <p>정보</p>
+          </div>
 
-              <div className={styles.test} dangerouslySetInnerHTML={{ __html: data.overview }} />
-              {}
-              {data.homepage && (
-                <>
-                  <div>홈페이지</div>
-                  <div dangerouslySetInnerHTML={{ __html: data.homepage }} />
-                </>
-              )}
+          <div className={styles.timeLine}>
+            <div className={styles.timeLineContainer}>
+              <div className={styles.timeLineicons}>
+                <TfiBook className={styles.infoIcons} />
+              </div>
+              <div className={styles.timeLinebody}>
+                <h4 className={styles.timeLineTitle}>{data.title}</h4>
+                <p className={styles.timeLineOverview} dangerouslySetInnerHTML={{ __html: data.overview }} />
+              </div>
+            </div>
+
+            <div className={styles.timeLineMapContainer}>
+              <div className={styles.timeLineiconsMap}>
+                <TfiLocationPin className={styles.infoIcons} />
+              </div>
+
+              <div className={styles.timeLineMapbody}>
+                <h4 className={styles.timeLineMapTitle}>위치</h4>
+
+                <div className={styles.addrWrapper}>
+                  <div className={styles.kakaoWrapper}>
+                    <Kakao Lat={data.mapy} Lng={data.mapx} />
+                  </div>
+
+                  <div className={styles.addrText}>
+                    <div className={styles.addr}>{data.addr1}</div>
+                    {data.homepage && (
+                      <div className={styles.homepage}>
+                        <div dangerouslySetInnerHTML={{ __html: data.homepage }} />
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.timeLineLikeContainer}>
+              <div className={styles.timeLineiconsLike}>
+                <TfiThumbUp className={styles.infoIcons} />
+              </div>
+              <div className={styles.timeLineLikebody}>
+                <div className={styles.timeLineLikeTitle}>
+                  {isLogin ? (
+                    <h4 className={styles.timeLineLikebtn} onClick={() => fetchLike()}>
+                      {subscribe}
+                    </h4>
+                  ) : (
+                    <h4 className={styles.timeLineLikebtn} onClick={() => alert('로그인을 해주세요!')}>
+                      123
+                    </h4>
+                  )}
+                </div>
+              </div>
             </div>
           </div>
-          <div>{data.addr1}</div>
-          {isLogin && <button onClick={() => fetchLike()}>{subscribe}</button>}
-          <Kakao Lat={data.mapy} Lng={data.mapx} />
         </div>
       ))}
     </section>
